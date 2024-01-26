6732c3d0f516215b6f2324cb2c1f97fc9f4a1567
AlexandreLG2022 <a.legal.contact@gmail.com>
2024-01-26 05:08:38 +0100

    :triangular_flag_on_post: Added sonner as toast library

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,15 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
 export default function HomePage() {
   return (<main>
     <h1 className="text-2xl font-bold">Hello World!</h1>
+    <Button onClick={() => {
+      toast.success('Lets go')
+    }}>
+      Toast!
+    </Button>
   </main>)
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/features/theme/ThemeProvider";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from 'sonner';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -24,6 +25,7 @@ export default function RootLayout({
         inter.className
       )}>
         <ThemeProvider attribute={'class'} defaultTheme={'system'} enableSystem>
+          <Toaster />
           <div className="flex flex-col h-full">
             <Header />
             <div className="flex-1 w-full max-w-xl py-6 m-auto pb-14">
